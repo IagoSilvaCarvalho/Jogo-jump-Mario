c7836ae8aa3826ac0b72c6b4f125a1b270adbb0e
Iago Carvalho <iago.isc.iago@gmail.com>
2022-07-21 17:11:12 -0300

    regras do jogo aplicada em JS

--- public/index.js
+++ public/index.js
@@ -1,5 +1,6 @@
-//pegar a classe da imagem gif
+//pegar a classe no CSS
 const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
 
 
 //Alerar a class no CSS para usar a animação pulo
@@ -11,7 +12,20 @@ const pulo = ()=>{
         mario.classList.remove('jump');
     }, 500)
 
-}
+};
+//criar loop para fim de game
+const loop = setInterval(()=>{
+    //pegar posicão do bloco para saber quando ira bater
+    const pipePosition = pipe.offsetLeft
+    //pegar altura do Mario para saber se ele bateu
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','')
+    //criar regra para quando o personagem bater no objeto o game parar
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition <= 85  ){
+        pipe.style.animation = 'none';
+        //condição para as imagens permanecerem onde o game parou
+        pipe.style.left = `${pipePosition}px`;
+    }
+},10 )
 
 
 
